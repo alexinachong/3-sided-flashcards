ba6da1c7b99176088a38a457575860268b978219
Alexina Chong <alexina.chong@gmail.com>
2018-07-10 17:05:42 -0700

    Update styling classes and primary component on App

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import FlashcardListContainer from './components/flashcard_list_container';
+import FlashcardPageContainer from './components/flashcard_page_container';
 
 class App extends Component {
+
   render() {
     return (
       <div className="container">
         <h1 className="header-text">Three-Sided Flashcards</h1>
-        <FlashcardListContainer />
+        <FlashcardPageContainer />
       </div>
     );
   }
