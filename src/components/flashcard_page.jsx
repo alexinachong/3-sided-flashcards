2850dcc7d68f8cf46f738da546c0caef330e3205
Alexina Chong <alexina.chong@gmail.com>
2018-07-17 23:50:55 -0700

    Update functions to reflect new flashcard count

--- src/components/flashcard_page.jsx
+++ src/components/flashcard_page.jsx
@@ -7,7 +7,7 @@ class FlashcardPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      flashcardNum: 1,
+      flashcardNum: 0,
       flashcardSide: 'phrase'
     };
 
@@ -24,22 +24,22 @@ class FlashcardPage extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    if (this.state.flashcardNum < Object.keys(this.props.flashcards).length) {
+    if (this.state.flashcardNum < Object.keys(this.props.flashcards).length - 1) {
       this.setState({
         flashcardNum: this.state.flashcardNum + 1
       });
     } else {
       this.setState({
-        flashcardNum: 1
+        flashcardNum: 0
       });
     }
   }
 
   goToPreviousCard(e) {
     e.preventDefault();
-    if (this.state.flashcardNum === 1) {
+    if (this.state.flashcardNum === 0) {
       this.setState({
-        flashcardNum: Object.keys(this.props.flashcards).length
+        flashcardNum: Object.keys(this.props.flashcards).length - 1
       });
     } else {
       this.setState({
@@ -84,7 +84,7 @@ class FlashcardPage extends React.Component {
     return (
       <div className="flashcard-page">
         <h2 className="deck-name">Russian Greetings</h2>
-        <p className="flashcard-count"><strong>{this.state.flashcardNum}</strong> of <strong>{Object.keys(this.props.flashcards).length}</strong></p>
+        <p className="flashcard-count"><strong>{this.state.flashcardNum + 1}</strong> of <strong>{Object.keys(this.props.flashcards).length}</strong></p>
         <FlashcardContent flashcard={this.props.flashcards[this.state.flashcardNum]} flashcardId={this.state.flashcardNum} flashcardSide={this.state.flashcardSide} flipUp={this.flipUp} flipDown={this.flipDown} />
         <div className="change-flashcard-buttons-container">
           <button onClick={this.goToPreviousCard} className="change-flashcard-button">PREVIOUS</button>
