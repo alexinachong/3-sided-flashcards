73186646eb99b7617b4fdd7f93da0fbcfd151f4d
Alexina Chong <alexina.chong@gmail.com>
2018-07-17 23:50:14 -0700

    Update flashcard count to begin at 0

--- src/reducers/flashcards_reducer.js
+++ src/reducers/flashcards_reducer.js
@@ -2,89 +2,89 @@ import { merge } from 'lodash';
 import { RECEIVE_FLASHCARD, RECEIVE_FLASHCARDS } from '../actions/flashcard_actions';
 
 let dataset = {
-  1:
+  0:
     {
-      "id": 1,
-      "phrase": "Доброе утро",
-      "definition": "Good morning",
-      "pronunciation_ipa": "dˈobrəjə ˈutrə",
-      "pronunciation_respelling": "Dobraye ootro"
-    },
-  2:
-    {
-      "id": 2,
-      "phrase": "Добрый день",
-      "definition": "Good afternoon",
-      "pronunciation_ipa": "dˈobrᵻj dʲˈenʲ",
-      "pronunciation_respelling": "Dobriy den'"
-    },
-  3:
-    {
-      "id": 3,
-      "phrase": "Добрый вечер",
-      "definition": "Good evening",
-      "pronunciation_ipa": "dˈobrᵻj vʲˈet͡ɕɪr",
-      "pronunciation_respelling": "Dobriy vyecher"
-    },
-  4:
-    {
-      "id": 4,
+      "id": 0,
       "phrase": "Здравствуйте",
       "definition": "Hello",
       "pronunciation_ipa": "zdrˈastvʊjtʲɪ",
       "pronunciation_respelling": "Zdrastvooyte"
     },
-  5:
+  1:
     {
-      "id": 5,
+      "id": 1,
       "phrase": "Привет!",
       "definition": "Hi!",
       "pronunciation_ipa": "prʲɪvʲˈet",
       "pronunciation_respelling": "Preevyet"
     },
-  6:
+  2:
     {
-      "id": 6,
+      "id": 2,
+      "phrase": "Доброе утро",
+      "definition": "Good morning",
+      "pronunciation_ipa": "dˈobrəjə ˈutrə",
+      "pronunciation_respelling": "Dobraye ootro"
+    },
+  3:
+    {
+      "id": 3,
+      "phrase": "Добрый день",
+      "definition": "Good afternoon",
+      "pronunciation_ipa": "dˈobrᵻj dʲˈenʲ",
+      "pronunciation_respelling": "Dobriy den'"
+    },
+  4:
+    {
+      "id": 4,
+      "phrase": "Добрый вечер",
+      "definition": "Good evening",
+      "pronunciation_ipa": "dˈobrᵻj vʲˈet͡ɕɪr",
+      "pronunciation_respelling": "Dobriy vyecher"
+    },
+  5:
+    {
+      "id": 5,
       "phrase": "Рад тебя видеть",
       "definition": "Nice to see you!",
       "pronunciation_ipa": "rˈat tʲɪbʲˈæ vʲˈidʲɪtʲ",
       "pronunciation_respelling": "Rat teebya veedet'"
     },
-  7:
+  6:
     {
-      "id": 7,
+      "id": 6,
       "phrase": "Как поживаешь?",
       "definition": "How are you?",
       "pronunciation_ipa": "kˈak pəʐᵻvˈajɪʂ?",
       "pronunciation_respelling": "Kak pazhivayesh?"
     },
-  8:
+  7:
     {
-      "id": 8,
+      "id": 7,
       "phrase": "Спасибо, прекрасно!",
       "definition": "Fine, thanks!",
       "pronunciation_ipa": "spɐsʲˈibə, prʲɪkrˈasnə",
       "pronunciation_respelling": "Spaseeba preekrasna"
     },
-  9:
+  8:
     {
-      "id": 9,
+      "id": 8,
       "phrase": "Неплохо!",
       "definition": "Not so bad",
       "pronunciation_ipa": "nʲɪplˈoxə",
       "pronunciation_respelling": "Neeploha"
     },
-  10:
+  9:
     {
-      "id": 10,
+      "id": 9,
       "phrase": "Как Вас зовут?",
       "definition": "What's your name?",
       "pronunciation_ipa": "kˈak vˈas zɐvˈut?",
       "pronunciation_respelling": "Kak vas zavoot?"
     },
-  11:
+  10:
     {
-      "id": 11,
+      "id": 10,
       "phrase": "Меня зовут...",
       "definition": "My name is...",
       "pronunciation_ipa": "mʲɪnʲˈæ zɐvˈut...",
