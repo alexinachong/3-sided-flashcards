13164fb1f74a7373803fbefb624e76843b20da48
Alexina Chong <alexina.chong@gmail.com>
2018-07-22 23:47:08 -0700

    Add basic structure for Quiz component

---
+++ src/components/quiz.jsx
@@ -0,0 +1,19 @@
+// Object.values(obj).map(key => key.hi)
+
+import React from 'react';
+
+class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="quiz-container">
+        
+      </div>
+    );
+  }
+}
+
+export default Quiz;
