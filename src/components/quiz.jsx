98721e55dd4530e043a441314cef64bb892345c9
Alexina Chong <alexina.chong@gmail.com>
2018-08-04 23:46:11 -0700

    Implement array shuffler for quiz

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -7,18 +7,39 @@ class Quiz extends React.Component {
     super(props);
 
     this.randomizeDefinitionOptions = this.randomizeDefinitionOptions.bind(this);
+    this.shuffleArray = this.shuffleArray.bind(this);
   }
 
   randomizeDefinitionOptions() {
     let allDefinitions = Object.values(this.props.flashcards).map(key => key.definition);
     let definitionOptions = [];
 
-    let optionOne = allDefinitions[Math.floor(Math.random() * allDefinitions.length)];
+    // let optionOne = allDefinitions[Math.floor(Math.random() * allDefinitions.length)];
 
     // select 3 from everything but solution, plus actual solution
     // randomize order of array
     // map over array to display below word
-    return optionOne;
+    // return optionOne;
+
+    let shuffledOptions = this.shuffleArray(allDefinitions);
+    return shuffledOptions;
+  }
+
+  shuffleArray(array) {
+    let newArray = [];
+
+    let randEl = 0;
+    let arrLength = array.length;
+    while (newArray.length < arrLength) {
+      console.log(arrLength);
+      let randElIdx = Math.floor(Math.random() * array.length);
+      randEl = array[randElIdx];
+      newArray.push(randEl);
+
+      array.splice(randElIdx, 1);
+    }
+
+    return newArray;
   }
 
   render() {
